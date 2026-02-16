2d6780d124f0ccf82ca0d5d6f585b225d8fe8704
Ranger <mnipk1243@gmail.com>
2026-02-17 00:42:27 +0500

    added ap chorh do sound

--- src/components/dashboard/Dino.jsx
+++ src/components/dashboard/Dino.jsx
@@ -1,7 +1,23 @@
 import ChromeDinoGame from "react-chrome-dino";
 import "../../styles/dino.css";
-
+import { useEffect } from "react";
+import {getApChorhdo} from "../../assets/soundStrings/sound";
+import getSoundString from "../../assets/soundStrings/sound";
 function Dino() {
+    useEffect(() => {
+        let audioComponents = document.querySelectorAll("audio")
+        let string = getSoundString();
+        for(let i = 0; i < audioComponents.length; i++){
+            audioComponents[i].src = `data:audio/mpeg;base64,${string}`
+        }
+        const hit = document.getElementById("offline-sound-hit");
+
+        if(hit){
+            let str = getApChorhdo()
+            hit.src = `data:audio/mpeg;base64,${str}`
+
+        }
+    }, [])
     return (
         <ChromeDinoGame />
     )
