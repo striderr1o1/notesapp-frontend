70d8aab8f3695e42dc4ecffb98c211f94f1065fe
Ranger <mnipk1243@gmail.com>
2026-04-21 23:04:33 +0500

    added basename in browserouter main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='notesapp-frontend'>
     <App />
   </BrowserRouter>
 )
